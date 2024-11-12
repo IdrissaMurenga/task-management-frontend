c1b00d543fdb89d7798c8a3c584203afc6e99c2c
idriss <idrissamurenga7@gmail.com>
2024-11-12 18:59:51 +0200

    chnaged a condition rendering on tasks page

--- app/pages/allTasks/page.tsx
+++ app/pages/allTasks/page.tsx
@@ -8,36 +8,39 @@ const AllTasks = () => {
   const { tasks, setTasks, loading, toggleComplete, clickToEditTask, editTask, deleteTask } = useData()
 
   const inCompletedTasks = tasks.filter(task => !task.completed)
-  
 
   return (
-    <>
-      {loading ? (
-        <Spinner size="xl" mx='10rem' my='8.2rem' />
-      ) :
-      (
-        <Reorder.Group axis='y' values={tasks} onReorder={setTasks}>
-          <Box bgColor='bg-secondary' px='0.2rem' pb='0.2rem' roundedBottom='0.875rem' >
-              <Box bgColor='bg-primary' p='1rem' roundedBottom='0.875rem' overflowY="auto" h='22rem'>
-                {inCompletedTasks.length > 0
-                  ?
-                    (                
-                      inCompletedTasks.map((task) => (
-                        <Reorder.Item key={task.id} value={task}>
-                          <TasksList key={task.id} task={task} toggleComplete={toggleComplete} clickToEditTask={clickToEditTask} editTask={editTask} deleteTask={deleteTask} />
-                        </Reorder.Item>
-                      ))
-                    )
-                  :
-                    (
-                      <Text py='8.2rem' fontSize='text-lg' textAlign='center'>No Tasks Added Yet</Text>
-                    )
-                }
-            </Box>
-          </Box>
-        </Reorder.Group> 
-      )}
-    </>
+    <Reorder.Group axis='y' values={tasks} onReorder={setTasks}>
+      <Box bgColor='bg-secondary' px='0.2rem' pb='0.2rem' roundedBottom='0.875rem' >
+        <Box bgColor='bg-primary' p='1rem' roundedBottom='0.875rem' overflowY="auto" h='22rem'>
+          {loading
+            ?
+              (
+                <Spinner size="xl" mx='10rem' my='8.2rem' />
+              )
+            : inCompletedTasks.length > 0 ?
+              (
+                inCompletedTasks.map((task) => (
+                  <Reorder.Item key={task.id} value={task}>
+                    <TasksList
+                      key={task.id}
+                      task={task}
+                      toggleComplete={toggleComplete}
+                      clickToEditTask={clickToEditTask}
+                      editTask={editTask}
+                      deleteTask={deleteTask}
+                    />
+                  </Reorder.Item>
+                ))
+              )
+            :
+              (
+                <Text py='8.2rem' fontSize='text-lg' textAlign='center'>No Tasks Added Yet</Text>
+              )
+          }
+        </Box>
+      </Box>
+    </Reorder.Group> 
   )
 }
 
